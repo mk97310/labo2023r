36823111703ae9dc0576d9c30a6f9c5fc2ac5d64
mk97310 <98096092+mk97310@users.noreply.github.com>
2023-07-05 17:42:33 -0300

    Update 621_DR_corregir_drifting.r

--- src/workflow-inicial/621_DR_corregir_drifting.r
+++ src/workflow-inicial/621_DR_corregir_drifting.r
@@ -13,7 +13,7 @@ require("yaml")
 PARAM <- list()
 PARAM$experimento <- "DR6210"
 
-PARAM$exp_input <- "CA6110_ML"
+PARAM$exp_input <- "CA6110"
 
 PARAM$variables_intrames <- TRUE # atencion esto esta en TRUE
 
