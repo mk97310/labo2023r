53917c2209b896be34b8cdd904f5912238cdf267
mk97310 <98096092+mk97310@users.noreply.github.com>
2023-07-07 23:58:51 -0300

    Update 641_TS_training_strategy.r

--- src/workflow-inicial/641_TS_training_strategy.r
+++ src/workflow-inicial/641_TS_training_strategy.r
@@ -18,21 +18,19 @@ PARAM$exp_input <- "FE6310"
 # me salteo los meses duros de pandemia, pero llego hasta 201907 en training
 # entreno en 18 meses
 
-PARAM$future <- c(202107)
+PARAM$future <- c(202106)
 PARAM$final_train <- c(
-  202105, 202104, 202103, 202102,
-  202101, 202012, 202011, 202010, 202009, 202008, 202002, 202001, 201912,
-  201911, 201910, 201909, 201908, 201907
-)
+  202104, 202103, 202102, 202101, 202012, 202011, 
+  202010, 202009, 202008, 202002, 202001, 201912, 
+  201911, 201910, 201909, 201908, 201907, 201906)
 
 PARAM$train$training <- c(
-  202103, 202102, 202101,
-  202012, 202011, 202010, 202009, 202008, 202002, 202001, 201912, 201911,
-  201910, 201909, 201908, 201907, 201906, 201905
-)
+  202102, 202101, 202012, 202011, 202010, 202009,
+  202008, 202002, 202001, 201912, 201911, 201910,
+  201909, 201908, 201907, 201906, 201905, 201904)
 
-PARAM$train$validation <- c(202104)
-PARAM$train$testing <- c(202105)
+PARAM$train$validation <- c(202103)
+PARAM$train$testing <- c(202104)
 
 # Atencion  0.1  de  undersampling de la clase mayoritaria,  los CONTINUA
 # 1.0 significa NO undersampling ,  0.1  es quedarse con el 10% de los CONTINUA
