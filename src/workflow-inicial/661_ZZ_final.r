d5b7341c27d52d168d7ac5b9ecc4f5332b34d3e8
mk97310 <98096092+mk97310@users.noreply.github.com>
2023-07-08 23:02:00 -0300

    Update 661_ZZ_final.r

--- src/workflow-inicial/661_ZZ_final.r
+++ src/workflow-inicial/661_ZZ_final.r
@@ -35,7 +35,7 @@ require("lightgbm")
 
 # Parametros del script
 PARAM <- list()
-PARAM$experimento <- "ZZ6610_202103"
+PARAM$experimento <- "ZZ6610_202102"
 PARAM$exp_input <- "HT6510"
 
 # Atencion, que cada modelos se procesa con 5 semillas, ajuste a SUS necesidades
