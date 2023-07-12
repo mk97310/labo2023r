01516d91bad0ed9301b6eddf57bde5b865e660f6
mk97310 <98096092+mk97310@users.noreply.github.com>
2023-07-11 22:05:03 -0300

    Add files via upload

---
+++ src/workflow-inicial/EX_601-RUN-Trainst-08.r
@@ -0,0 +1,17 @@
+# Corrida general del workflow
+options(error = function() {
+  traceback(20)
+  options(error = NULL)
+  stop("exiting after script error")
+})
+
+# corrida de cada paso del workflow
+# primeros pasos, relativamente rapidos
+source("~/labo2023r/src/workflow-inicial/EX_611_CA_reparar_dataset.r")
+source("~/labo2023r/src/workflow-inicial/EX_621_DR_corregir_drifting.r")
+source("~/labo2023r/src/workflow-inicial/EX_631_FE_historia.r")
+source("~/labo2023r/src/workflow-inicial/Trainst-08.r")
+
+# ultimos pasos, muy lentos
+source("~/labo2023r/src/workflow-inicial/EX_651_HT_lightgbm.r")
+source("~/labo2023r/src/workflow-inicial/EX_661_ZZ_final.r")
