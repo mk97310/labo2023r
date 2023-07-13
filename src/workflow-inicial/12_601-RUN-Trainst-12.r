332110be7baace886e6cac586adadef1d493ef96
mk97310 <98096092+mk97310@users.noreply.github.com>
2023-07-12 22:35:42 -0300

    Update 12_601-RUN-Trainst-12.r

--- src/workflow-inicial/12_601-RUN-Trainst-12.r
+++ src/workflow-inicial/12_601-RUN-Trainst-12.r
@@ -7,11 +7,11 @@ options(error = function() {
 
 # corrida de cada paso del workflow
 # primeros pasos, relativamente rapidos
-("~/labo2023r/src/workflow-inicial/611_CA_reparar_dataset.r")
-source("~/labo2023r/src/workflow-inicial/621_DR_corregir_drifting.r")
-source("~/labo2023r/src/workflow-inicial/631_FE_historia.r")
-source("~/labo2023r/src/workflow-inicial/Trainst-12.r")
+#("~/labo2023r/src/workflow-inicial/611_CA_reparar_dataset.r")
+#source("~/labo2023r/src/workflow-inicial/621_DR_corregir_drifting.r")
+#source("~/labo2023r/src/workflow-inicial/631_FE_historia.r")
+source("~/labo2023r/src/workflow-inicial/12_Trainst-12.r")
 
 # ultimos pasos, muy lentos
-source("~/labo2023r/src/workflow-inicial/651_HT_lightgbm.r")
-source("~/labo2023r/src/workflow-inicial/661_ZZ_final.r")
+source("~/labo2023r/src/workflow-inicial/12_651_HT_lightgbm.r")
+source("~/labo2023r/src/workflow-inicial/12_661_ZZ_final.r")
