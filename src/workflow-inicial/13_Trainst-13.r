4664c78674d018c2d60160f23f86e4c2ea277dc5
mk97310 <98096092+mk97310@users.noreply.github.com>
2023-07-12 22:41:55 -0300

    Update 13_Trainst-13.r

--- src/workflow-inicial/13_Trainst-13.r
+++ src/workflow-inicial/13_Trainst-13.r
@@ -6,8 +6,8 @@ require("data.table")
 require("yaml")
 # Parametros del script
 PARAM <- list()
-PARAM$experimento <- "TS6410"
-PARAM$exp_input <- "FE6310"
+PARAM$experimento <- "TS6410_13"
+PARAM$exp_input <- "FE6310_EX"
 
 # me salteo los meses duros de pandemia, pero llego hasta 201907 en training
 # entreno en 18 meses
