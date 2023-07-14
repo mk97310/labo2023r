7690a4bd6594326c7d6dfbc167443049fccd2b9e
mk97310 <98096092+mk97310@users.noreply.github.com>
2023-07-14 06:38:48 -0300

    Update and rename 631_FE_historia_.r to 631_FE_historia_202107.r

--- src/workflow-inicial/631_FE_historia_202107.r
+++ src/workflow-inicial/631_FE_historia_202107.r
@@ -17,9 +17,9 @@ require("lightgbm")
 
 # Parametros del script
 PARAM <- list()
-PARAM$experimento <- "FE6310"
+PARAM$experimento <- "FE6310_202107"
 
-PARAM$exp_input <- "DR6210"
+PARAM$exp_input <- "DR6210_202107"
 
 PARAM$lag1 <- TRUE
 PARAM$lag2 <- TRUE
