7ed1f8f57c9033e37a89e770295e15756dff5a89
mk97310 <98096092+mk97310@users.noreply.github.com>
2023-07-14 06:38:03 -0300

    Update and rename 621_DR_corregir_drifting.r to 621_DR_corregir_drifting_202107.r

--- src/workflow-inicial/621_DR_corregir_drifting_202107.r
+++ src/workflow-inicial/621_DR_corregir_drifting_202107.r
@@ -11,9 +11,9 @@ require("yaml")
 
 # Parametros del script
 PARAM <- list()
-PARAM$experimento <- "DR6210"
+PARAM$experimento <- "DR6210_202107"
 
-PARAM$exp_input <- "CA6110"
+PARAM$exp_input <- "CA6110_202107"
 
 PARAM$variables_intrames <- TRUE # atencion esto esta en TRUE
 
