857ba5dda7d5d2e02d8e9e69f60aa7ef3f9921ec
mk97310 <98096092+mk97310@users.noreply.github.com>
2023-07-14 06:39:23 -0300

    Update and rename 651_HT_lightgbm.r to 651_HT_lightgbm_202107.r

--- src/workflow-inicial/651_HT_lightgbm_202107.r
+++ src/workflow-inicial/651_HT_lightgbm_202107.r
@@ -27,9 +27,9 @@ options(error = function() {
 
 # Parametros del script
 PARAM <- list()
-PARAM$experimento <- "HT6510_202104"
+PARAM$experimento <- "HT6510_202107"
 
-PARAM$exp_input <- "TS6410_202104"
+PARAM$exp_input <- "TS6410_202107"
 
 # En caso que se haga cross validation, se usa esta cantidad de folds
 PARAM$lgb_crossvalidation_folds <- 5
