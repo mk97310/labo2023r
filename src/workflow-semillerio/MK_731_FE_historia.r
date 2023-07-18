5ec1d2e759dadc2f128378390f329ed4d82a17bd
mk97310 <98096092+mk97310@users.noreply.github.com>
2023-07-18 08:02:06 -0300

    Update and rename z731_FE_historia.r to MK_731_FE_historia.r

--- src/workflow-semillerio/MK_731_FE_historia.r
+++ src/workflow-semillerio/MK_731_FE_historia.r
@@ -17,9 +17,9 @@ require("lightgbm")
 
 # Parametros del script
 PARAM <- list()
-PARAM$experimento <- "FE7310"
+PARAM$experimento <- "MK_FE7310_E1" # le anulé el Random Forest
 
-PARAM$exp_input <- "DR7210"
+PARAM$exp_input <- "MK_DR7210"
 
 PARAM$lag1 <- TRUE
 PARAM$lag2 <- TRUE
@@ -44,12 +44,12 @@ PARAM$Tendencias2$ratioavg <- FALSE
 PARAM$Tendencias2$ratiomax <- FALSE
 
 
-PARAM$RandomForest$run <- TRUE
+PARAM$RandomForest$run <- FALSE 
 PARAM$RandomForest$num.trees <- 20
 PARAM$RandomForest$max.depth <- 4
 PARAM$RandomForest$min.node.size <- 1000
 PARAM$RandomForest$mtry <- 40
-PARAM$RandomForest$semilla <- 102191 # cambiar por la propia semilla
+PARAM$RandomForest$semilla <- 387763 # cambiar por la propia semilla
 
 
 # varia de 0.0 a 2.0, si es 0.0 NO se activan
@@ -57,7 +57,7 @@ PARAM$CanaritosAsesinos$ratio <- 0.0
 # desvios estandar de la media, para el cutoff
 PARAM$CanaritosAsesinos$desvios <- 4.0
 # cambiar por la propia semilla
-PARAM$CanaritosAsesinos$semilla <- 200177
+PARAM$CanaritosAsesinos$semilla <- 387763
 
 PARAM$home <- "~/buckets/b1/"
 # FIN Parametros del script
